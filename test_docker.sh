b9a4c22cc18129a54e7ebdff3a3c76f45107d49e
Luca Pinello <lucapinello@gmail.com>
2017-03-22 22:08:26 -0400

    Update test_docker.sh

--- test_docker.sh
+++ test_docker.sh
@@ -12,12 +12,12 @@ docker  run -v  $PWD/crispor_genomes:/crisporWebsite/genomes lucapinello/crispor
 docker  run  -v  $PWD/crispor_genomes:/crisporWebsite/genomes -v $PWD/crispor_data:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot crispor.py hg19 crispor_input.fa crispor_output.tsv --satMutDir=./
 
 #simulate filtering
-docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot head -n 10 REGION_1_satMutOligos.tsv > REGION_1_satMutOligos_filtered.tsv 
+docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot bash -c "head -n 10 REGION_1_satMutOligos.tsv > REGION_1_satMutOligos_filtered.tsv" 
 
 #create filtered files for the experiment
-docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetAmplicons.tsv -o 2.1,2.2,2.3 > CRISPRessoPooled_amplicons.tsv
-docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetAmplicons.tsv -o 2.3 > CRISPRessoCounts_sgRNA.tsv
-docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetPrimers.tsv -o 2.1,2.2,2.3,2.4,2.5,2.6,2.7 > REGION_1_ontargetPrimers_filtered.tsv
+docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot bash -c "join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetAmplicons.tsv -o 2.1,2.2,2.3 > CRISPRessoPooled_amplicons.tsv"
+docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot bash -c "join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetAmplicons.tsv -o 2.3 > CRISPRessoCounts_sgRNA.tsv"
+docker run -v $PWD/crispor_data/:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot bash -c "join -1 1 -2 1 REGION_1_satMutOligos_filtered.tsv REGION_1_ontargetPrimers.tsv -o 2.1,2.2,2.3,2.4,2.5,2.6,2.7 > REGION_1_ontargetPrimers_filtered.tsv"
 
 #CRISPRESSO TESTING
 docker  run -v $PWD/amplicon_data:/DATA -w /DATA lucapinello/crispor_crispresso_nat_prot  CRISPResso -r1 reads1.fastq.gz -r2 reads2.fastq.gz -a AATGTCCCCCAATGGGAAGTTCATCTGGCACTGCCCACAGGTGAGGAGGTCATGATCCCCTTCTGGAGCTCCCAACGGGCCGTGGTCTGGTTCATCATCTGTAAGAATGGCTTCAAGAGGCTCGGCTGTGGTT --g TGAACCAGACCACGGCCCGT -s 20 -q 30 -n BCL11A_exon2
